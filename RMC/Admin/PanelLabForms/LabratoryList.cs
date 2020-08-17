dbd5a2180630d9335c11e101dae097d0ca14d235
rafael <rafaelllparaynooo23@gmail.com>
2020-08-17 22:03:09 +0800

    showing data in grid in lab

--- RMC/Admin/PanelLabForms/LabratoryList.cs
+++ RMC/Admin/PanelLabForms/LabratoryList.cs
@@ -1,5 +1,6 @@
 ﻿using RMC.Admin.PanelLabForms.Dialogs;
 using RMC.Admin.PanelLabForms.PanelsSettings;
+using RMC.Database.Controllers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -15,16 +16,30 @@ namespace RMC.Admin.PanelLabForms
   
     public partial class LabratoryList : Form
     {
-     
+        LaboratoryController laboratoryController = new LaboratoryController();
         public LabratoryList()
         {
             InitializeComponent();
+            loadGrid();
         }
 
         private void btnAddItem_Click(object sender, EventArgs e)
         {
             AddEditLab form = new AddEditLab();
             form.ShowDialog();
+        }
+
+        private async void loadGrid()
+        {
+            DataSet ds = await laboratoryController.getDataSet();
+            refreshGrid(ds);
+        }
+
+        private void refreshGrid(DataSet ds)
+        {
+            dgLabList.DataSource = "";
+            dgLabList.DataSource = ds.Tables[0];
+            dgLabList.AutoResizeColumns();
         }
     }
 }
