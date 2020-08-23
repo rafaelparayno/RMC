55725baa2d1a129c7aa760e9b788ff8cec91e4c8
rafael <rafaelllparaynooo23@gmail.com>
2020-08-23 17:46:54 +0800

    add validation

--- RMC/Patients/PanelsDetails/PanelPatientDetails.Designer.cs
+++ RMC/Patients/PanelsDetails/PanelPatientDetails.Designer.cs
@@ -30,29 +30,29 @@
         {
             this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtLn = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtMn = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtfn = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.cbGender = new System.Windows.Forms.ComboBox();
             this.label8 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtCn = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
             this.label11 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.cbStatus = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -62,11 +62,11 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.txtLn);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtMn);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtfn);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
@@ -77,12 +77,12 @@
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             // 
-            // textBox2
+            // txtLn
             // 
-            this.textBox2.Location = new System.Drawing.Point(507, 41);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(198, 24);
-            this.textBox2.TabIndex = 5;
+            this.txtLn.Location = new System.Drawing.Point(507, 41);
+            this.txtLn.Name = "txtLn";
+            this.txtLn.Size = new System.Drawing.Size(198, 24);
+            this.txtLn.TabIndex = 5;
             // 
             // label3
             // 
@@ -94,12 +94,12 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Last Name:";
             // 
-            // textBox1
+            // txtMn
             // 
-            this.textBox1.Location = new System.Drawing.Point(269, 41);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(198, 24);
-            this.textBox1.TabIndex = 3;
+            this.txtMn.Location = new System.Drawing.Point(269, 41);
+            this.txtMn.Name = "txtMn";
+            this.txtMn.Size = new System.Drawing.Size(198, 24);
+            this.txtMn.TabIndex = 3;
             // 
             // label2
             // 
@@ -111,12 +111,12 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Middle Name:";
             // 
-            // txtName
+            // txtfn
             // 
-            this.txtName.Location = new System.Drawing.Point(18, 42);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(198, 24);
-            this.txtName.TabIndex = 1;
+            this.txtfn.Location = new System.Drawing.Point(18, 42);
+            this.txtfn.Name = "txtfn";
+            this.txtfn.Size = new System.Drawing.Size(198, 24);
+            this.txtfn.TabIndex = 1;
             // 
             // label1
             // 
@@ -131,7 +131,7 @@
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.dateTimePicker1);
-            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.txtAge);
             this.groupBox2.Controls.Add(this.label4);
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
@@ -151,13 +151,13 @@
             this.dateTimePicker1.TabIndex = 7;
             this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
-            // textBox3
+            // txtAge
             // 
-            this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(507, 41);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(198, 24);
-            this.textBox3.TabIndex = 6;
+            this.txtAge.Enabled = false;
+            this.txtAge.Location = new System.Drawing.Point(507, 41);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(198, 24);
+            this.txtAge.TabIndex = 6;
             // 
             // label4
             // 
@@ -181,10 +181,10 @@
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.comboBox1);
-            this.groupBox3.Controls.Add(this.textBox5);
+            this.groupBox3.Controls.Add(this.cbStatus);
+            this.groupBox3.Controls.Add(this.cbGender);
             this.groupBox3.Controls.Add(this.label8);
-            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.txtCn);
             this.groupBox3.Controls.Add(this.label5);
             this.groupBox3.Controls.Add(this.label7);
             this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
@@ -196,12 +196,17 @@
             this.groupBox3.TabIndex = 4;
             this.groupBox3.TabStop = false;
             // 
-            // textBox5
+            // cbGender
             // 
-            this.textBox5.Location = new System.Drawing.Point(507, 41);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(198, 24);
-            this.textBox5.TabIndex = 8;
+            this.cbGender.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cbGender.Location = new System.Drawing.Point(18, 37);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(198, 24);
+            this.cbGender.TabIndex = 9;
             // 
             // label8
             // 
@@ -213,12 +218,12 @@
             this.label8.TabIndex = 7;
             this.label8.Text = "Civil Status";
             // 
-            // textBox4
+            // txtCn
             // 
-            this.textBox4.Location = new System.Drawing.Point(269, 41);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(198, 24);
-            this.textBox4.TabIndex = 6;
+            this.txtCn.Location = new System.Drawing.Point(269, 41);
+            this.txtCn.Name = "txtCn";
+            this.txtCn.Size = new System.Drawing.Size(198, 24);
+            this.txtCn.TabIndex = 6;
             // 
             // label5
             // 
@@ -249,10 +254,11 @@
             this.button1.TabIndex = 5;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox4
             // 
-            this.groupBox4.Controls.Add(this.textBox7);
+            this.groupBox4.Controls.Add(this.txtAddress);
             this.groupBox4.Controls.Add(this.label11);
             this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox4.Location = new System.Drawing.Point(0, 216);
@@ -263,16 +269,16 @@
             this.groupBox4.TabIndex = 7;
             this.groupBox4.TabStop = false;
             // 
-            // textBox7
+            // txtAddress
             // 
-            this.textBox7.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.txtAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox7.Location = new System.Drawing.Point(12, 37);
-            this.textBox7.Multiline = true;
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(687, 37);
-            this.textBox7.TabIndex = 10;
+            this.txtAddress.Location = new System.Drawing.Point(18, 37);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(687, 37);
+            this.txtAddress.TabIndex = 10;
             // 
             // label11
             // 
@@ -284,21 +290,23 @@
             this.label11.TabIndex = 0;
             this.label11.Text = "Address:";
             // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Male",
-            "Female"});
-            this.comboBox1.Location = new System.Drawing.Point(18, 37);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(198, 24);
-            this.comboBox1.TabIndex = 9;
-            // 
             // errorProvider1
             // 
             this.errorProvider1.ContainerControl = this;
+            // 
+            // cbStatus
+            // 
+            this.cbStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbStatus.FormattingEnabled = true;
+            this.cbStatus.Items.AddRange(new object[] {
+            "Single",
+            "Married",
+            "Separated",
+            "Widowed"});
+            this.cbStatus.Location = new System.Drawing.Point(507, 41);
+            this.cbStatus.Name = "cbStatus";
+            this.cbStatus.Size = new System.Drawing.Size(198, 24);
+            this.cbStatus.TabIndex = 10;
             // 
             // PanelPatientDetails
             // 
@@ -331,28 +339,28 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtLn;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtMn;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtfn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtAge;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtCn;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox txtAddress;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbGender;
         private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ComboBox cbStatus;
     }
 }
--- RMC/Patients/PanelsDetails/PanelPatientDetails.cs
+++ RMC/Patients/PanelsDetails/PanelPatientDetails.cs
@@ -23,7 +23,62 @@ namespace RMC.Patients.PanelsDetails
             int yrNow = DateTime.Now.Year;
             int bdate = dateTimePicker1.Value.Year;
             int age = yrNow - bdate;
-            textBox3.Text = age.ToString();
+            txtAge.Text = age.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isValid())
+            {
+                MessageBox.Show("PLease Complete Required Field");
+                return;
+            }
+        }
+
+        private bool isValid()
+        {
+            bool isValid = true;
+            errorProvider1.Clear();
+
+            isValid = (txtfn.Text != "") && isValid;
+            SetErrorShowNoText(ref txtfn, "Please Input a Value");
+
+            isValid = (txtLn.Text != "") && isValid;
+            SetErrorShowNoText(ref txtLn, "Please Input a Value");
+
+            isValid = (txtCn.Text != "") && isValid;
+            SetErrorShowNoText(ref txtCn, "Please Input a Value");
+
+            isValid = (txtAddress.Text != "") && isValid;
+            SetErrorShowNoText(ref txtAddress, "Please Input a Value");
+
+            isValid = (txtAge.Text != "") && isValid;
+            SetErrorShowNoText(ref txtAge, "Please Input a Value");
+
+            isValid = (cbGender.SelectedIndex > -1) && isValid;
+            setErrorCbNoValue(ref cbGender, "Please Select A value");
+
+            isValid = (cbStatus.SelectedIndex > -1) && isValid;
+            setErrorCbNoValue(ref cbStatus, "Please Select A value");
+
+
+            return isValid;
+        }
+
+        private void setErrorCbNoValue(ref ComboBox cb,string msg)
+        {
+            if(cb.SelectedIndex == -1)
+            {
+                errorProvider1.SetError(cb, msg);
+            }
+        }
+
+        private void SetErrorShowNoText(ref TextBox tb, string msg)
+        {
+            if(tb.Text == "")
+            {
+                errorProvider1.SetError(tb, msg);
+            }
         }
     }
 }
